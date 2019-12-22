0ee4bb1982aa49af2938b5c7c3f6fe5223f7ddea
Jesus <jesusperiago@gmail.com>
2019-12-22 22:23:10 +0100

    Prueba de two way binding inportanto el FormsModule

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -7,5 +7,21 @@ import { Component } from "@angular/core";
 })
 
 export class PersonasComponent {
+  agregarPersona=false;
+  agregarPersonaStatus = "No se ha agregado ninguna persona";
+  tituloPersona = "Prueba";
+  constructor(){
+    setTimeout(
+      () => {
+        this.agregarPersona = true
+      }
+      ,3000);
+  }
+  onCrearPersona(){
+    this.agregarPersonaStatus = "Persona agregada";
+}
 
+  onModificarPersona(event: Event) {
+    this.tituloPersona = (<HTMLInputElement>event.target).value;
+  }
 }
