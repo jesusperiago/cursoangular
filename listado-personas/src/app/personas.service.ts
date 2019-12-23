b5e2481e90b1534f7e00bd7d8221e1b3bf22d819
Jesus <jesusperiago@gmail.com>
2019-12-23 17:11:35 +0100

    Inyectamos servicio loggin dentro del servicio personas

--- listado-personas/src/app/personas.service.ts
+++ listado-personas/src/app/personas.service.ts
@@ -1,9 +1,15 @@
 import {Persona} from "./persona.model";
+import {LoggingService} from "./LoggingService.service";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class PersonasService {
   personas: Persona[] = [new Persona("Juan", "Perez"), new Persona("Laura", "Juarez")];
 
+  constructor(private logginService: LoggingService){}
+
   agregarPersona(persona: Persona){
+    this.logginService.enviaMensajeAConsola("Agregamos persona:" + persona.nombre +" "+ persona.apellido );
     this.personas.push(persona);
   }
 }
