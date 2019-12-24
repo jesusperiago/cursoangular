eeec2337f8b1d92ef524b336cdd4febcb0d6607b
Jesus <jesusperiago@gmail.com>
2019-12-24 18:05:48 +0100

    Ejercicio03 Routas hijas

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {PersonasComponent} from "./personas/personas.component";
 import {FormularioComponent} from "./personas/formulario/formulario.component";
+import {ErrorComponent} from "./error/error.component";
 
 const routes: Routes = [
     {path: '', component: PersonasComponent},
@@ -12,7 +13,9 @@ const routes: Routes = [
           {path: 'agregar', component: FormularioComponent},
           {path: ':id', component: FormularioComponent}
         ]
-    }
+    },
+    {path: '**', component: ErrorComponent}
+
 
   ]
 ;
