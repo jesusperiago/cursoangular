3a197fefb8ebe323b4ec982583220722ec8e8ecd
Jesus <jesusperiago@gmail.com>
2019-12-24 17:53:30 +0100

    Ejercicio03 Routas hijas

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {PersonasComponent} from "./personas/personas.component";
 import {FormularioComponent} from "./personas/formulario/formulario.component";
 
 const routes: Routes = [
-  {path: '', component: PersonasComponent},
-  {path: 'personas', component: PersonasComponent},
-  {path: 'personas/agregar', component: FormularioComponent},
-  {path: 'personas/:id', component: FormularioComponent}
-];
+    {path: '', component: PersonasComponent},
+    {
+      path: 'personas', component: PersonasComponent,
+      children:
+        [
+          {path: 'agregar', component: FormularioComponent},
+          {path: ':id', component: FormularioComponent}
+        ]
+    }
+
+  ]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(
-      routes
-    )
+    routes
+  )
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
