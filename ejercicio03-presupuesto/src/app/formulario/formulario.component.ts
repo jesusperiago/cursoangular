f0b49927a03c660e963dba788e78c24f73e00aa9
Jesus <jesusperiago@gmail.com>
2019-12-24 15:52:30 +0100

    Ejercicio03 validacion de formulario y ngClass

--- ejercicio03-presupuesto/src/app/formulario/formulario.component.ts
+++ ejercicio03-presupuesto/src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IngresoService} from "../ingresos/ingreso.service";
 import {EgresoService} from "../egresos/egreso.service";
 import {IngresoModel} from "../ingresos/ingreso.model";
@@ -11,22 +11,24 @@ import {EgresoModel} from "../egresos/egreso.model";
 })
 export class FormularioComponent implements OnInit {
 
-  tipo:string="ingresoOperacion";
-  descripcionInput:string;
-  valorInput:number;
+  tipo: string = "ingresoOperacion";
+  descripcionInput: string;
+  valorInput: number;
 
-  constructor(private ingresoService:IngresoService, private egresoService:EgresoService) { }
+  constructor(private ingresoService: IngresoService, private egresoService: EgresoService) {
+  }
 
   ngOnInit() {
   }
-  tipoOperacion(evento){
+
+  tipoOperacion(evento) {
     this.tipo = evento.target.value;
   }
 
-  agregarValor(){
-    if (this.tipo === "ingresoOperacion"){
+  agregarValor() {
+    if (this.tipo === "ingresoOperacion") {
       this.ingresoService.ingresos.push(new IngresoModel(this.descripcionInput, this.valorInput));
-    }else{
+    } else {
       this.egresoService.egreso.push(new EgresoModel(this.descripcionInput, this.valorInput));
     }
   }
