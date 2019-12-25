0510233a6cbf2d374a1f747f7f08f6d2d89dbc6f
Jesus <jesusperiago@gmail.com>
2019-12-25 23:59:44 +0100

    Creacion del servicio de clientes

--- control-clientes/src/environments/environment.prod.ts
+++ control-clientes/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: "AIzaSyDqwHnTDhmX1pGSsEhhTOVRAXczW1PhUj8",
+    authDomain: "control-clientes-bf4d4.firebaseapp.com",
+    databaseURL: "https://control-clientes-bf4d4.firebaseio.com",
+    projectId: "control-clientes-bf4d4",
+    storageBucket: "control-clientes-bf4d4.appspot.com",
+    messagingSenderId: "342022701889",
+    appId: "1:342022701889:web:121ad69164109dd4b1d7c0",
+    measurementId: "G-EWD751LZKT"
+  }
 };
