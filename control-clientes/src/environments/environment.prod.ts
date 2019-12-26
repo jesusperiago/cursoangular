b3316903f5f2ec1bda54da96768f715b4df80347
jesusperiago <jesusperiago@gmail.com>
2019-12-26 01:55:40 +0100

    Update environment.prod.ts

--- control-clientes/src/environments/environment.prod.ts
+++ control-clientes/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firestore: {
-    apiKey: "AIzaSyDqwHnTDhmX1pGSsEhhTOVRAXczW1PhUj8",
-    authDomain: "control-clientes-bf4d4.firebaseapp.com",
-    databaseURL: "https://control-clientes-bf4d4.firebaseio.com",
-    projectId: "control-clientes-bf4d4",
-    storageBucket: "control-clientes-bf4d4.appspot.com",
-    messagingSenderId: "342022701889",
-    appId: "1:342022701889:web:121ad69164109dd4b1d7c0",
-    measurementId: "G-EWD751LZKT"
+    apiKey: "firebase",
+    authDomain: "firebase",
+    databaseURL: "firebase",
+    projectId: "firebase",
+    storageBucket: "firebase",
+    messagingSenderId: "firebase",
+    appId: "firebase",
+    measurementId: "firebase"
   }
 };
